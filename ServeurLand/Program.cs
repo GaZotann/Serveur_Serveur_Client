5e44f2a3f8cff6f2149f51ed0cb185d3afacf0e7
GaZotann <gazotan.minecraft@gmail.com>
2022-01-13 14:47:24 +0100

    Code du main

--- ServeurLand/Program.cs
+++ ServeurLand/Program.cs
@@ -6,7 +6,21 @@ namespace ServeurLand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mode;
+            Console.WriteLine("Tapez 1 pour le serveur1, tapez 2 pour le serveur2: ");
+            mode = Int32.Parse(Console.ReadLine());
+            switch (mode)
+            {
+                case 1:
+                    Console.WriteLine("Démarrage du serveur 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Démarrage du serveur 2");
+                    break;
+                default:
+                    Console.WriteLine("Il faut mettre 1 ou 2... Fin du programme");
+                    return;
+            }
         }
     }
 }
