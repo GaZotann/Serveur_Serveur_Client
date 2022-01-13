294264cdc55d6c672c5c780e0d14141733e0ca42
GaZotann <gazotan.minecraft@gmail.com>
2022-01-13 13:07:32 +0100

    Création du main et des dossiers pour le projet

---
+++ ServeurLand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServeurLand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
