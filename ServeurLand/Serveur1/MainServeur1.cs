c469eaab1e66977ef1ae542ee0fb72173c83119c
GaZotann <gazotan.minecraft@gmail.com>
2022-02-08 11:55:08 +0100

    Server receive message

--- ServeurLand/Serveur1/MainServeur1.cs
+++ ServeurLand/Serveur1/MainServeur1.cs
@@ -11,63 +11,17 @@ namespace ServeurLand.Serveur1
 {
     public class MainServeur1
     {
-        public static void main()
+        public static void main() //serveur d'envoie 
         {
+            using Socket s = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
             try
             {
-                int port = 9099;
-                AsyncService service = new AsyncService(port);
-                service.Run();
 
             }
-            catch(Exception ex)
+            catch(Exception e)
             {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        public class AsyncService
-        {
-            private IPAddress iPAdress;
-            private int port;
-            public AsyncService(int port)
-            {
-            }
-
-            public async void Run()
-            {
-                TcpListener listener = new TcpListener(this.iPAdress, this.port);
-                listener.Start();
-                Console.WriteLine("Serveur up sur le port: " + this.port);
-                while (true)
-                {
-                    try
-                    {
-                        TcpClient tcpClient = await listener.AcceptTcpClientAsync();
-                        Task t = Process(tcpClient);
-                        await t;
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-            }
-            private async Task Process(TcpClient tcp)
-            {
-                String ClientEndPoint = tcp.Client.RemoteEndPoint.ToString();
-                Console.WriteLine("Connection request from " + ClientEndPoint);
-            }
-            private static string Response(String request)
-            {
-
-            }
-            private static Average(double[] vals)
-            {
-
-            }
-            private static double Minimum(double[] vals)
-            {
-
+                Console.WriteLine("Error");
             }
         }
     }
--- ServeurLand/Serveur2/MainServeur2.cs
+++ ServeurLand/Serveur2/MainServeur2.cs
@@ -1,16 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ServeurLand.Serveur2
 {
     public class MainServeur2
     {
-        public static void main()
+        public static void main() //serveur d'écoute
         {
+            IPHostEntry ips = Dns.GetHostEntry(""); //on choppe l'adresse ip du DNS 
+            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp); //on créer un socket 
+            listener.Bind(new IPEndPoint(ips.AddressList[0], 9199)); //on dit à l'os d'accepter des requetes de connections
 
+            listener.Listen(5); // on laisse 5 écoutes avant de rejeter 
+
+            byte[] lenbuf = new byte[sizeof(int)];
+            
+            while (true)
+            {
+                try
+                {
+                    using Socket request = listener.Accept(); // on accept la connection
+                    int red = request.Receive(lenbuf);
+
+                    if (red != lenbuf.Length)// on vérifie que les tailles sont bien les même
+                        throw new Exception();
+
+                    int messagelen = BitConverter.ToInt32(lenbuf);//on convertie la taille en int
+
+                    byte[] buffer = new byte[messagelen]; //on créé un buffer de la bonne taille 
+
+                    red = request.Receive(buffer);//on récupère le message
+
+                    if (red != lenbuf.Length) //on vérifie les taille
+                        throw new Exception();
+
+                    String command = Encoding.UTF8.GetString(buffer);//on met ce qu'il y a dans le buffer dans un string 
+
+                    //Todo command
+                    Console.WriteLine($"J'ai recu: {command}");
+
+                    request.Send(new byte[] { 0 });//on revoie 0 si tout s'est bien passé 
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
         }
     }
 }
